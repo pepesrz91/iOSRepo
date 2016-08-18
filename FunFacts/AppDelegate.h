e427194ee3047236e86ebcb9b712b7d2f7ec7bf4
Pepe <Krakenn@joses-imac.local>
2016-08-18 18:09:04 -0500

    Initial Commit

---
+++ FunFacts/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FunFacts
+//
+//  Created by Jose Juan Suarez on 8/18/16.
+//  Copyright © 2016 Jose Juan Suarez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FunFacts/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FunFacts
+//
+//  Created by Jose Juan Suarez on 8/18/16.
+//  Copyright © 2016 Jose Juan Suarez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
